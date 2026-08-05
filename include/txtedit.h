4c32361210d6f4299bd72e529bd1d946baae05ed
Victor Arch-Linux <victorleroux92@gmail.com>
2026-08-05 13:41:20 +0200

    Initial commit

--- include/txtedit.h
+++ include/txtedit.h
@@ -2,5 +2,16 @@
 # define TXTEDIT_H 
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef struct s_file{
+
+	char *data;
+	struct s_file *previous;
+	struct s_file *next;
+	size_t  position;
+} t_file;
+
 
 #endif
--- src/txtedit.c
+++ src/txtedit.c
@@ -1,6 +1,17 @@
 #include "txtedit.h"
+#include <fcntl.h>
 
-int main(void)
+int main(int argc,char **argv)
 {
-	printf("test");
+	(void)argc;
+	int fd = open(argv[1],O_RDONLY);
+	char buffer[4096];
+	int bytes = read(fd,buffer,sizeof(buffer));
+	while(bytes > 0)
+	{
+	write(1,buffer,bytes);
+	bytes = read(fd,buffer,4096);
+	}
+	return 0;
+
 }
