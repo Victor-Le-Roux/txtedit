66c6dad8c5f4c28be785f80307dd5ae25ac91e63
vle-roux <victorleroux92@gmail.com>
2026-08-10 19:14:12 +0200

    creation de liste chaine qui recupere tout le fichier

--- include/txtedit.h
+++ include/txtedit.h
@@ -7,12 +7,15 @@
 #include <termios.h>
 #include <stdlib.h>
 
+#define SIZE_BUFFER 4096
+
 typedef struct s_file{
 
 	char *data;
 	struct s_file *previous;
 	struct s_file *next;
 	size_t  position;
+	size_t	data_size;
 } t_file;
 
 
--- src/txtedit.c
+++ src/txtedit.c
@@ -1,88 +1,114 @@
 #include "txtedit.h"
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
-int	main(void)
+void create_node(t_file *file)
 {
-	struct termios	old_term;
-	struct termios	raw_term;
-	char			c;
-	char			seq[2];
-	char			*items[] = {
-		"Open file",
-		"Create file",
-		"Settings",
-		"Quit"
-	};
-	int				selected;
-	int				i;
-	int				running;
+	t_file *node = malloc(sizeof(t_file));
+	file->next = node;
 
-	selected = 0;
-	running = 1;
-
-	tcgetattr(STDIN_FILENO, &old_term);
-
-	raw_term = old_term;
-	raw_term.c_lflag &= ~(ICANON | ECHO);
-
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_term);
-
-	while (running)
+	node->data = NULL;
+	node->previous = file;
+	node->next = NULL;
+	node->position = 0;
+	node->data_size = 0;
+}
+size_t strlen_line(char *buffer)
+{
+	size_t i = 0;
+	while(buffer[i] && buffer[i]  != '\n')
+		i++;
+	return i;
+}
+void put_line(char *buffer,t_file *file)
+{
+	size_t buffer_line = strlen_line(buffer);
+	size_t i = 0;
+	file->data = malloc(buffer_line + 1);
+	while(i < buffer_line)
 	{
-		printf("\x1b[2J");
-		printf("\x1b[H");
+		file->data[i]=buffer[i];
+		i++;
+	}
+	file->data[i] = '\0';
+	file->data_size = buffer_line;
+	create_node(file);
+}
+static size_t	put_complete_lines(char *buffer, size_t used, t_file **file)
+{
+	size_t	i;
+	size_t	start;
 
-		printf("=== MENU ===\r\n\r\n");
-
-		i = 0;
-		while (i < 4)
+	i = 0;
+	start = 0;
+	while (i < used)
+	{
+		if (buffer[i] == '\n')
 		{
-			if (i == selected)
-			{
-				printf("\x1b[7m");
-				printf("  %s  ", items[i]);
-				printf("\x1b[0m");
-			}
-			else
-				printf("  %s  ", items[i]);
-
-			printf("\r\n");
-			i++;
+			buffer[i] = '\0';
+			put_line(buffer + start, *file);
+			*file = (*file)->next;
+			start = i + 1;
 		}
+		i++;
+	}
+	return (start);
+}
 
-		if (read(STDIN_FILENO, &c, 1) != 1)
+static size_t	move_remaining(char *buffer, size_t used, size_t start)
+{
+	size_t	i;
+
+	i = 0;
+	while (start + i < used)
+	{
+		buffer[i] = buffer[start + i];
+		i++;
+	}
+	return (i);
+}
+
+void	create_file(int fd, t_file *file)
+{
+	char		buffer[SIZE_BUFFER + 1];
+	ssize_t		bytes;
+	size_t		used;
+	size_t		start;
+
+	used = 0;
+	while (1)
+	{
+		bytes = read(fd, buffer + used, SIZE_BUFFER - used);
+		if (bytes <= 0)
 			break;
+		used += (size_t)bytes;
 
-		if (c == '\x1b')
-		{
-			if (read(STDIN_FILENO, &seq[0], 1) != 1)
-				continue;
-			if (read(STDIN_FILENO, &seq[1], 1) != 1)
-				continue;
+		start = put_complete_lines(buffer, used, &file);
+		used = move_remaining(buffer, used, start);
 
-			if (seq[0] == '[')
-			{
-				if (seq[1] == 'A')
-				selected--;
-				else if (seq[1] == 'B')
-					selected++;
-			}
-		}
-		else if (c == '\r' || c == '\n')
-			running = 0;
+		if (used == SIZE_BUFFER)
+			return;
+	}
+	if (used > 0)
+	{
+		buffer[used] = '\0';
+		put_line(buffer, file);
+	}
+}
 
-		if (selected < 0)
-			selected = 3;
-		else if (selected > 3)
-			selected = 0;
+int	main(int argc ,char **argv)
+{
+	t_file *file;
+	file = malloc(sizeof(t_file));
+	int fd = open("test.txt",O_RDONLY);
+	create_file(fd,file);
+	while(file->data != NULL)
+	{
+		printf("%s\n",file->data);
+		file = file->next;
 	}
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_term);
-
-	printf("\x1b[2J\x1b[H");
-	printf("Tu as choisi : %s\n", items[selected]);
-
-	return (0);
 }
